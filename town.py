0e5d4af64430f5f54a16a11b2c200a63a1c97250
Abhi Temmanaboyina <abhit@gn.csd.local>
2017-10-16 13:34:11 -0400

    test

---
+++ town.py
@@ -0,0 +1,16 @@
+#Author: Abhi and Ahad
+
+
+def hub():
+	print("Welcome to Noodle Town!")
+	hubquestions = input("Where would you like to go. The shop, battle arena, save area, or to the noodle microwave? (1) (2) (3) (4) ")
+ if hubquestions = "1"
+	print("All right. Let's go!")
+	s.shop()
+elif hubquestions = "2"
+	print("Hope you don't die.")
+	b.arena
+elif hubquestions = "3"
+	print("Oke Doike.")
+
+
