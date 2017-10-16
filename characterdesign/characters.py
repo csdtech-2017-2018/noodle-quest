9212dfaf8bc594b5798ddc02eb64283380ab78f3
Luke Whiteside <lukew@gn.csd.local>
2017-10-16 13:31:15 -0400

    my

---
+++ characterdesign/characters.py
@@ -0,0 +1,24 @@
+import colors as c
+author="Matu and Patlu"
+
+class Thing():
+	dmg=None
+	hpo=None
+	hp=None
+	agi=None
+	deff=None
+	dpeno=None
+	dpen=None
+	spp=None
+	lvl=None
+	hpp=None
+
+class Player(Thing):
+	noodles=0
+	name=None
+
+class Foe(Thing):
+	mname="Test mob name"
+
+
+
