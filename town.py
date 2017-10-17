d02a2a0edfb804f0ba894a58bf2e87c332c01850
Abhi Temmanaboyina <abhit@gn.csd.local>
2017-10-17 09:44:58 -0400

    town.py

--- town.py
+++ town.py
@@ -6,11 +6,17 @@ def hub():
 	hubquestions = input("Where would you like to go. The shop, battle arena, save area, or to the noodle microwave? (1) (2) (3) (4) ")
  if hubquestions = "1"
 	print("All right. Let's go!")
-	s.shop()
+	shop()
 elif hubquestions = "2"
 	print("Hope you don't die.")
-	b.arena
+	battle arena()
 elif hubquestions = "3"
 	print("Oke Doike.")
-
-
+	save area()
+elif hubquestion = "4"
+	print("Wow! You are brave to go the mistical noodle microwave.")
+	noodle microwave()
+else:
+	print("I do not understand.")
+	sleep(1)
+	hub()
