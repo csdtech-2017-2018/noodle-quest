f86b695af41d821f5f4d782c03f29661cf474cf7
Luke Whiteside <lukew@gn.csd.local>
2017-10-18 13:09:53 -0400

    hi

--- characterdesign/characters.py
+++ characterdesign/characters.py
@@ -22,4 +22,18 @@ class Foe(Thing):
 	mname="Test mob name"
 
 
-
+class Luke_potential_energy_user(Player):
+	dmg=35
+	hpo=100
+	agl=25
+	deff=15
+	dpeno=4
+	dpen=2
+	spp=1
+	lvl=1
+	hpp=2
+	hlp=40
+class The_Goonslayer(Player):
+	dmg=25
+	hpo=70
+	
